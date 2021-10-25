2dceb892d992e77b870b1a1d7caf2995bdf3d518
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-10-25 12:10:32 +0700

    re-arrange bottom row

--- keyboards/handwired/gpr/keymaps/default/keymap.c
+++ keyboards/handwired/gpr/keymaps/default/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NV_SCLN, CTL_QUO,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, _______, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   _______, _______, _______, _______,
+    KC_LCTL, _______, _______, KC_LGUI, KC_LALT, LOWER,   RAISE,   KC_SPC,  KC_ENT,  _______, _______, _______
   ),
 
   [_LOWER] = LAYOUT(
