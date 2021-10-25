a19fb8603d77c92321d62fbc7ccc3fd1390f7128
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-10-25 15:29:36 +0700

    add alt tab to thumb

--- keyboards/handwired/gpr/keymaps/default/keymap.c
+++ keyboards/handwired/gpr/keymaps/default/keymap.c
@@ -25,6 +25,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define CTL_QUO RCTL_T(KC_QUOT)
 #define LSF_CPS TD(TD_LSFT_CAPS)
 #define RSF_CPS TD(TD_RSFT_CAPS)
+#define ALT_TAB A(KC_TAB)
+#define ALS_TAB LSA(KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
@@ -38,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_HOME, KC_END,  _______,
-    _______, RESET,   DEBUG,   _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______, RESET,   DEBUG,   _______, _______, _______, _______, ALS_TAB, ALT_TAB, KC_VOLD, KC_VOLU, KC_MPLY
   ),
 
   [_RAISE] = LAYOUT(
