e5054c352deeddbfc2d3c1880f58ed190a5971ad
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-10-25 12:07:45 +0700

    add momentary left and right ctrl key

--- keyboards/handwired/gpr/keymaps/default/keymap.c
+++ keyboards/handwired/gpr/keymaps/default/keymap.c
@@ -10,11 +10,13 @@ enum gpr_layers {
 #define LOWER TT(_LOWER)
 #define RAISE TT(_RAISE)
 #define NV_SCLN LT(_NAV, KC_SCLN)
+#define CTL_ESC LCTL_T(KC_ESC)
+#define CTL_QUO RCTL_T(KC_QUOT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NV_SCLN, KC_QUOT,
+    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NV_SCLN, CTL_QUO,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
     KC_LCTL, _______, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   _______, _______, _______, _______,
   ),
