26c2294043e5ac7f47903217fec876af0f0acb7a
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-10-25 19:51:42 +0700

    change lower layer to symbol layer

--- keyboards/handwired/gpr/keymaps/default/keymap.c
+++ keyboards/handwired/gpr/keymaps/default/keymap.c
@@ -2,12 +2,12 @@
 
 enum gpr_layers {
   _BASE,
-  _LOWER,
+  _SYMBOL,
   _RAISE,
   _NAV,
 };
 
-#define LOWER TT(_LOWER)
+#define SYMBOL TT(_SYMBOL)
 #define RAISE TT(_RAISE)
 #define NV_SCLN LT(_NAV, KC_SCLN)
 #define CTL_ESC LCTL_T(KC_ESC)
@@ -21,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NV_SCLN, CTL_QUO,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, _______, RAI_GUI, KC_LGUI, KC_LALT, LOWER,   RAISE,   KC_SPC,  KC_ENT,  _______, _______, _______
+    KC_LCTL, _______, RAI_GUI, KC_LGUI, KC_LALT, SYMBOL,  RAISE,   KC_SPC,  KC_ENT,  _______, _______, _______
   ),
 
-  [_LOWER] = LAYOUT(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_HOME, KC_END,  _______,
-    _______, RESET,   DEBUG,   _______, _______, _______, _______, ALS_TAB, ALT_TAB, KC_VOLD, KC_VOLU, KC_MPLY
+  [_SYMBOL] = LAYOUT(
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
+    _______, _______, _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_MINS, KC_EQL,  KC_UNDS, KC_PLUS, KC_PIPE,
+    RESET,   DEBUG,   _______, _______, _______, _______, _______, ALS_TAB, ALT_TAB, _______, _______, _______
   ),
 
   [_RAISE] = LAYOUT(
