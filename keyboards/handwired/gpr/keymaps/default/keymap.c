92ef2476c0cc95c274c78cd17858d9ea2e122d90
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-10-26 08:05:05 +0700

    move space and enter key position

--- keyboards/handwired/gpr/keymaps/default/keymap.c
+++ keyboards/handwired/gpr/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NV_SCLN, CTL_QUO,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, FN,      SYM_GUI, KC_LGUI, KC_LALT, SYMBOL,  _______, KC_SPC,  KC_ENT,  _______, _______, _______
+    KC_LCTL, FN,      SYM_GUI, KC_LGUI, KC_LALT, SYMBOL,  KC_SPC,  KC_ENT,  _______, _______, _______, _______
   ),
 
   [_SYMBOL] = LAYOUT(
