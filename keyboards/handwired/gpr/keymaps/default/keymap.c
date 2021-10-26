b38ac3900d28da8978a5644e2757c0c5dd94b787
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-10-26 09:39:55 +0700

    add cut copy paste key

--- keyboards/handwired/gpr/keymaps/default/keymap.c
+++ keyboards/handwired/gpr/keymaps/default/keymap.c
@@ -14,13 +14,16 @@ enum gpr_layers {
 #define CTL_ESC LCTL_T(KC_ESC)
 #define CTL_QUO RCTL_T(KC_QUOT)
 #define SYM_GUI LM(_SYMBOL, MOD_LGUI)
+#define CUT C(KC_X)
+#define COPY C(KC_C)
+#define PASTE C(KC_V)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NV_SCLN, CTL_QUO,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, FN,      SYM_GUI, KC_LGUI, KC_LALT, SYMBOL,  KC_SPC,  KC_ENT,  NAV,     _______, _______, _______
+    KC_LCTL, FN,      SYM_GUI, KC_LGUI, KC_LALT, SYMBOL,  KC_SPC,  KC_ENT,  NAV,     CUT,     COPY,    PASTE
   ),
 
   [_SYMBOL] = LAYOUT(
