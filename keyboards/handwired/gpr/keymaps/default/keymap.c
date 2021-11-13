868975c98f2bdca62597519edce6ba498e66415c
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-11-13 09:37:31 +0700

    add basic mouse key functionality

--- keyboards/handwired/gpr/keymaps/default/keymap.c
+++ keyboards/handwired/gpr/keymaps/default/keymap.c
@@ -58,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FN] = LAYOUT(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_WH_D, KC_WH_U,
     _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, _______,
-    KC_PSCR, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT
+    KC_PSCR, _______, _______, _______, _______, KC_BTN3, KC_BTN2, KC_BTN1, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT
   ),
 
   [_NAV] = LAYOUT(
