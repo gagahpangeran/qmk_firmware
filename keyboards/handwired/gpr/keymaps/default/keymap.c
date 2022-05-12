18805e898e24b5bd907eecd6363a32f36695f954
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2022-05-12 10:44:50 +0700

    change capslock to backslash

--- keyboards/handwired/gpr/keymaps/default/keymap.c
+++ keyboards/handwired/gpr/keymaps/default/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NV_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_CAPS,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
     KC_F13,  FN,      SYM_GUI, KC_LGUI, KC_LALT, SYMBOL,  SYM_SPC, CTL_ENT, SFT_NAV, CUT,     COPY,    PASTE
   ),
 
