63abac4aff99057314c0a91a84385b17c8fafcc8
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-05-07 23:07:58 +0700

    clean up config file

--- keyboards/handwired/gpr/config.h
+++ keyboards/handwired/gpr/config.h
@@ -1,15 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    GPR
-#define PRODUCT         KB01
-
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
@@ -17,16 +8,9 @@
 /* key matrix pins */
 #define MATRIX_ROW_PINS { F6, B3, B2, B6 }
 #define MATRIX_COL_PINS { F4, F5, B5, B4, E6, D7, C6, D4, D0, D1, D2, D3 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
-
-/* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 0
-#endif
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
@@ -47,13 +31,5 @@
 #define MOUSEKEY_WHEEL_DELAY 100
 #define MOUSEKEY_WHEEL_MAX_SPEED 2
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 0
-
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
 
 #endif
